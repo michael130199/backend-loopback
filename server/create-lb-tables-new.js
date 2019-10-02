8149a45bec772d124babbcce6fc08213a74bf332
michael130199 <michael130199@gmail.com>
2019-10-02 11:46:56 -0400

    No recuerdo que hice

--- server/create-lb-tables-new.js
+++ server/create-lb-tables-new.js
@@ -1,6 +1,16 @@
 var server = require('./server');
 var ds = server.dataSources.db;
-var lbTables = ["role", "user"];
+var lbTables = ["user", "contact","message"];
+//,
+  // "news",
+  // "event", 
+  // "service", 
+  // "role", 
+  // "news_tag",
+  // "tag",
+  // "registry",
+  // "file1"
+
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
